2ac7e0276616ec78651f9aa1d4956ac48e0d83e4
Claire Pate <clairepate@Claires-Air.T-mobile.com>
2018-02-11 13:01:51 -0500

    updated conection info

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var db = require("./models");
 
 // var PORT = 3000;
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI;
 
 // Initialize Express
 var app = express();
@@ -32,7 +32,7 @@ app.use(express.static("public"));
 // Connect to the Mongo DB
 mongoose.Promise = Promise;
 mongoose.connect("mongodb://localhost/scrapperdb", {
-    // useMongoClient: true
+    useMongoClient: true
 });
 
 
