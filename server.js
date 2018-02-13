bb2e98632328d3fc766d24888bd060b2896579fa
Claire Pate <clairepate@Claires-Air.attlocal.net>
2018-02-13 17:27:05 -0500

    updated connection

--- server.js
+++ server.js
@@ -12,9 +12,6 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = process.env.PORT || 3000;
-
-
 
 // Initialize Express
 var app = express();
@@ -42,38 +39,112 @@ if (process.env.MONGODB_URI) {
 }
 
 
+// A GET request to scrape the echo website
 app.get("/scrape", function (req, res) {
     // First, we grab the body of the html with request
-    axios.get("http://www.washingtonpost.com/").then(function (response) {
+    request("http://www.echojs.com/", function (error, response, html) {
         // Then, we load that into cheerio and save it to $ for a shorthand selector
-        var $ = cheerio.load(response.data);
-
+        var $ = cheerio.load(html);
         // Now, we grab every h2 within an article tag, and do the following:
         $("article h2").each(function (i, element) {
+
             // Save an empty result object
             var result = {};
 
             // Add the text and href of every link, and save them as properties of the result object
-            result.title = $(this)
-                .children("a")
-                .text();
-            result.link = $(this)
-                .children("a")
-                .attr("href");
+            result.title = $(this).children("a").text();
+            result.link = $(this).children("a").attr("href");
 
-            // Create a new Article using the `result` object built from scraping
-            db.Article.create(result)
-                .then(function (dbArticle) {
-                    // View the added result in the console
-                    console.log(dbArticle);
-                })
-                .catch(function (err) {
-                    // If an error occurred, send it to the client
-                    return res.json(err);
-                });
+            // Using our Article model, create a new entry
+            // This effectively passes the result object to the entry (and the title and link)
+            var entry = new Article(result);
+
+            // Now, save that entry to the db
+            entry.save(function (err, doc) {
+                // Log any errors
+                if (err) {
+                    console.log(err);
+                }
+                // Or log the doc
+                else {
+                    console.log(doc);
+                }
+            });
+
         });
-
-        // If we were able to successfully scrape and save an Article, send a message to the client
-        res.send("Scrape Complete");
     });
+    // Tell the browser that we finished scraping the text
+    res.send("Scrape Complete");
+});
+
+
+// This will get the articles we scraped from the mongoDB
+app.get("/articles", function (req, res) {
+    // Grab every doc in the Articles array
+    Article.find({}, function (error, doc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+        // Or send the doc to the browser as a json object
+        else {
+            res.json(doc);
+        }
+    });
+});
+
+// Grab an article by it's ObjectId
+app.get("/articles/:id", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    Article.findOne({ "_id": req.params.id })
+        // ..and populate all of the notes associated with it
+        .populate("note")
+        // now, execute our query
+        .exec(function (error, doc) {
+            // Log any errors
+            if (error) {
+                console.log(error);
+            }
+            // Otherwise, send the doc to the browser as a json object
+            else {
+                res.json(doc);
+            }
+        });
+});
+
+
+// Create a new Comment or replace an existing Comment
+app.post("/articles/:id", function (req, res) {
+    // Create a new Comment and pass the req.body to the entry
+    var newComment = new Comment(req.body);
+
+    // And save the new Comment the db
+    newComment.save(function (error, doc) {
+        // Log any errors
+        if (error) {
+            console.log(error);
+        }
+        // Otherwise
+        else {
+            // Use the article id to find and update it's Comment
+            Article.findOneAndUpdate({ "_id": req.params.id }, { "comment": doc._id })
+                // Execute the above query
+                .exec(function (err, doc) {
+                    // Log any errors
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        // Or send the document to the browser
+                        res.send(doc);
+                    }
+                });
+        }
+    });
+});
+
+var PORT = process.env.PORT || 3000;
+// Listen on port 3000
+app.listen(PORT, function () {
+    console.log("App running on port 3000!");
 });
